f756c4aa16a29fe08bede6f3dbebba9aaf1c316a
Marcus Arthur Klitzke Andriani <eng.marcusarthur@yahoo.com>
2020-08-17 22:16:53 -0300

    Update desafio003.py
    almemanuel, can you see this? I am testing Github, learning. Please, give me a return if you are seeing this. This comment is on a fork that I create from yours. Please cite this. Thank you!

--- programs/desafios/desafio003.py
+++ programs/desafios/desafio003.py
@@ -1,3 +1,5 @@
 n1 = int(input('Digite um numero: '))
 n2 = int(input('Digite outro numero: '))
-print('A soma entre {} e {} é {}' .format(n1, n2, n1 + n2))
+print('A soma entre {} e {} eh {}' .format(n1, n2, n1 + n2))
+#You can chance the printing values order just informing the position between the braces, like this:
+print('{2} menos {0} eh igual a {1}' .format(n1, n2, n1 + n2))
