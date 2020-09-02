1ddf0cfd127fe4d06e86779561340126a780f9dd
Marcus Arthur Klitzke Andriani <marcusandriani@gmail.com>
2020-09-02 13:40:03 -0300

    removendo exercicio

--- programs/desafios/desafio003.py
+++
@@ -1,5 +0,0 @@
-n1 = int(input('Digite um numero: '))
-n2 = int(input('Digite outro numero: '))
-print('A soma entre {} e {} eh {}' .format(n1, n2, n1 + n2))
-#You can chance the printing values order just informing the position between the braces, like this:
-print('{2} menos {0} eh igual a {1}' .format(n1, n2, n1 + n2))
